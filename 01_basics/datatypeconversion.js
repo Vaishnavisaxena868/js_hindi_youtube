672d61c3ce0f5ca206a7b1b2547416036061d678
Vaishnavisaxena868 <149888910+Vaishnavisaxena868@users.noreply.github.com>
2025-05-03 15:02:13 +0000

    commit dataype

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,28 @@
+let num = null
+
+console.log(typeof score);
+
+let valueInNumber = Number(num)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+let value = undefined
+let valueInNumber2 = Number(value)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+let loggedin = 1
+let booleanloggedin = Boolean(loggedin)
+console.log(typeof booleanloggedin);
+console.log(booleanloggedin);
+
+//1=>true
+//0=>false
+//""=>false
+
+
+
+
+
+
